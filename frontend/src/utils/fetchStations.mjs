253e9ef7b00e612325b38caff47515fab2c2dec1
Amani Hamad <amanihd2017@gmail.com>
2025-04-11 17:47:54 +0300

    Update splash screen and app icon,finished filters logic,adjust the code of main map , adjust the charging station icon and it color

--- frontend/src/utils/fetchStations.mjs
+++ frontend/src/utils/fetchStations.mjs
@@ -1,9 +1,11 @@
 // fetchStations.mjs
-import fetch from 'node-fetch';
-import fs from 'fs';
+import fetch from "node-fetch";
+import fs from "fs";
 
 const fetchStations = async () => {
-  const response = await fetch('https://api.openchargemap.io/v3/poi/?output=json&countrycode=JO&maxresults=100&key=a164f27a-d27a-456a-a7fe-0fd483025c3f');
+  const response = await fetch(
+    "https://api.openchargemap.io/v3/poi/?output=json&countrycode=JO&maxresults=100&key=a164f27a-d27a-456a-a7fe-0fd483025c3f"
+  );
   const data = await response.json();
 
   const filteredData = data.map((station, index) => ({
@@ -13,17 +15,18 @@ const fetchStations = async () => {
     latitude: station.AddressInfo.Latitude,
     longitude: station.AddressInfo.Longitude,
     cost: station.UsageCost || null,
-    amenities: station.Amenities || [],
-    plugType: station.Connections?.map((conn) => conn.ConnectionType?.Title),
+    amenities: station.MetadataValues || [],
+    plugType:
+      station.Connections?.map((conn) => conn.ConnectionType?.Title) || [],
     plugScore: station.UserComments?.[0]?.Rating || null,
     parking: station.NumberOfPoints || null,
-    isFast: station.Connections?.some(conn => conn.PowerKW >= 22),
+    isFast: station.Connections?.some((conn) => conn.PowerKW >= 22),
     numConnections: station.Connections?.length || 0,
     isAvailable: station.StatusType?.IsOperational ?? null,
   }));
 
-  fs.writeFileSync('./stations.json', JSON.stringify(filteredData, null, 2));
-  console.log('Data saved to stations.json');
+  fs.writeFileSync("./stations.json", JSON.stringify(filteredData, null, 2));
+  console.log("Data saved to stations.json");
 };
 
 fetchStations();
